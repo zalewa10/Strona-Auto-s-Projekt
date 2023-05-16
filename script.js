78b67b7f2c644cecde0b16111788fc3a9758d26b
Krzysztof Zal <pilkarz4a@gmail.com>
2023-05-16 12:51:53 +0200

    16.05 mapa i poprawki v2

--- script.js
+++ script.js
@@ -10,12 +10,29 @@ var locations = [
   ["Ogrody - parking ul. Nowina", 52.416835, 16.878214],
   ["Os.Kwiatowe - parking Netto", 52.378642, 16.823928],
   ["Os.Kwiatowe - parking kościół", 52.383611, 16.820688],
-  ["Skórzewo - Biuro i Sala Wykładowa AUTO-S", 52.400547, 16.784312],
+  ["Biuro i Sala Wykładowa AUTO-S, ul.Jesienna 18, Skórzewo", 52.400547, 16.784312],
   ["Dąbrówka - parking stacja Orlen", 52.381938, 16.744593],
   ["Dąbrowa - parking Chata Polska", 52.396823, 16.757426],
 ];
+var osm = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var map = L.map('map', {
+  // true by default, false if you want a wild map
+  sleep: true,
+  // time(ms) for the map to fall asleep upon mouseout
+  sleepTime: 750,
+  // time(ms) until map wakes on mouseover
+  wakeTime: 750,
+  // defines whether or not the user is prompted oh how to wake map
+  sleepNote: true,
+  // allows ability to override note styling
+  sleepNoteStyle: { color: 'red' },
+  // should hovering wake the map? (clicking always will)
+  hoverToWake: false,
+  // opacity (between 0 and 1) of inactive map
+  sleepOpacity: .7
 
-var map = L.map('map').setView([52.402651, 16.826466], 12);
+}).setView([52.402651, 16.826466], 12).addLayer(L.tileLayer(osm));
+
 mapLink =
   '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer(
